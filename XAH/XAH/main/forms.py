a4b46145eadc44a34edfacb29525410d6333f193
Дастан Байтурсынов <dastan211298@gmail.com>
2019-05-13 17:05:57 +0600

    added serializers

--- XAH/XAH/main/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from markdownx.fields import MarkdownxFormField
-
-class PostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    photo = forms.ImageField()
-    text = MarkdownxFormField()
-    category = forms.IntegerField()
--- XAH/XAH/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('post_create/', views.post_create, name="post_create"),
-    path('store/', views.store, name='store'),
-]
